6a90847524132c401f819a196c25635461ef7cec
Kariny Abrahao <kariny_abrahao@hotmail.com>
2024-11-17 18:03:07 -0300

    serverTCP comentado

--- serverTCP.c
+++ serverTCP.c
@@ -8,7 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-char *ROOT="arquivos";
+char *ROOT="arquivos"; // diretorio dos arquivos
 
 void error(const char *msg)
 {
@@ -19,34 +19,45 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-		char line[2048];
-		int sockfd, newsockfd, portno;
-		socklen_t clilen;
-		struct sockaddr_in serv_addr, cli_addr;
+		char line[2048]; // Buffer de dados recebidos do cliente
+		int sockfd, newsockfd, portno; // socket servidor, socket cliente(especifico), porta
+		socklen_t clilen; // tamanho da estrutura do endereço do cliente
+		struct sockaddr_in serv_addr, cli_addr; // endereços IP do servidor e do cliente
 		int n;
-		if (argc < 2) {
+
+		if (argc < 2) { // verifica se a porta foi passada como argumento
 				fprintf(stderr,"ERROR, no port provided\n");
 				exit(1);
 		}
+
+        // Criando o socket TCP
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if (sockfd < 0) 
 				error("ERROR opening socket");
-		bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+		bzero((char *) &serv_addr, sizeof(serv_addr)); // Zerando a estrutura serv_addr
 		portno = atoi(argv[1]);
 		serv_addr.sin_family = AF_INET;
 		serv_addr.sin_addr.s_addr = INADDR_ANY;
 		serv_addr.sin_port = htons(portno);
+
+        // Associa o socket à porta e endereço especificados
 		if (bind(sockfd, (struct sockaddr *) &serv_addr,
 								sizeof(serv_addr)) < 0) 
 				error("ERROR on binding");
 
+        // Coloca o servidor em modo de escuta
 		listen(sockfd,5);
 		clilen = sizeof(cli_addr);
 
 		int count = 0;
+        // O servidor aceita a conexão de um cliente(bloqueia até que um cliente se conecte)
+        // Quando a conexão é estabelecida, um novo socket (newsockfd) é criado para se comunicar com o cliente
 	    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 		if (newsockfd < 0) error("ERROR on accept");
 		
+
+        // Lê os dados enviados pelo cliente e responde com um HTML simples
 		while(1) {
             n = read(newsockfd, &line[count], 1);
 
