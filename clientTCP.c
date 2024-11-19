0fbcfe549a0184146e77192fdfbdf67d4196c376
Kariny Abrahao <kariny_abrahao@hotmail.com>
2024-11-19 11:02:58 -0300

    adicionando funcao de calculo do hash no clientTCP.c

--- clientTCP.c
+++ clientTCP.c
@@ -10,6 +10,57 @@
 
 #define BUFFER_SIZE 1024  // Tamanho do buffer para receber os dados
 
+void calculate_md5(const char *filepath, unsigned char *result) {
+    int filefd = open(filepath, O_RDONLY);
+    if (filefd < 0) {
+        perror("Erro ao abrir o arquivo para calcular MD5");
+        exit(1);
+    }
+
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    if (mdctx == NULL) {
+        perror("Erro ao criar contexto MD5");
+        close(filefd);
+        exit(1);
+    }
+
+    if (EVP_DigestInit_ex(mdctx, EVP_md5(), NULL) != 1) {
+        perror("Erro ao inicializar MD5");
+        EVP_MD_CTX_free(mdctx);
+        close(filefd);
+        exit(1);
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    while ((bytes_read = read(filefd, buffer, BUFFER_SIZE)) > 0) {
+        if (EVP_DigestUpdate(mdctx, buffer, bytes_read) != 1) {
+            perror("Erro ao atualizar MD5");
+            EVP_MD_CTX_free(mdctx);
+            close(filefd);
+            exit(1);
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Erro ao ler o arquivo para calcular MD5");
+        EVP_MD_CTX_free(mdctx);
+        close(filefd);
+        exit(1);
+    }
+
+    unsigned int md_len;
+    if (EVP_DigestFinal_ex(mdctx, result, &md_len) != 1) {
+        perror("Erro ao finalizar MD5");
+        EVP_MD_CTX_free(mdctx);
+        close(filefd);
+        exit(1);
+    }
+
+    EVP_MD_CTX_free(mdctx);
+    close(filefd);
+}
+
 int main(int argc, char **argv) {
     // Verificando se o IP do servidor e a porta foram passados como argumentos
     if (argc != 3) {
@@ -53,66 +104,34 @@ int main(int argc, char **argv) {
     // Abrindo o arquivo para escrita no diretório arquivos_cliente
     char filepath[1024];
     snprintf(filepath, sizeof(filepath), "arquivos_cliente/arquivo_recebido");
-    int filefd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    int filefd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (filefd < 0) {
         perror("Erro ao abrir o arquivo para escrita");
         close(client_sock);
         exit(1);
     }
 
-    // Início da função para calcular o hash MD5 do arquivo recebido
-    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    if (mdctx == NULL) {
-        perror("Erro ao criar contexto MD5");
-        close(filefd);
-        close(client_sock);
-        exit(1);
-    }
-
-    if (EVP_DigestInit_ex(mdctx, EVP_md5(), NULL) != 1) {
-        perror("Erro ao inicializar MD5");
-        EVP_MD_CTX_free(mdctx);
-        close(filefd);
-        close(client_sock);
-        exit(1);
-    }
-
-    // Iniciando a medição do tempo de download
-    clock_gettime(CLOCK_MONOTONIC, &start_time);
-
+    // Recebe os dados do servidor em blocos e escreve no arquivo
     ssize_t bytes_received;
     while ((bytes_received = read(client_sock, buffer, BUFFER_SIZE)) > 0) {
         // Medindo o tempo de recepção dos bytes
-        struct timespec temp_start, temp_end;
-        clock_gettime(CLOCK_MONOTONIC, &temp_start);
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
 
         total_bytes_received += bytes_received;
 
-        clock_gettime(CLOCK_MONOTONIC, &temp_end);
-        time_taken += (temp_end.tv_sec - temp_start.tv_sec) + (temp_end.tv_nsec - temp_start.tv_nsec) / 1e9;
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        time_taken += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
 
         if (write(filefd, buffer, bytes_received) != bytes_received) {
             perror("Erro ao escrever no arquivo");
-            EVP_MD_CTX_free(mdctx);
-            close(filefd);
-            close(client_sock);
-            exit(1);
-        }
-        if (EVP_DigestUpdate(mdctx, buffer, bytes_received) != 1) {
-            perror("Erro ao atualizar MD5");
-            EVP_MD_CTX_free(mdctx);
             close(filefd);
             close(client_sock);
             exit(1);
         }
     }
 
-    // Finalizando a medição do tempo de download
-    clock_gettime(CLOCK_MONOTONIC, &end_time);
-
     if (bytes_received < 0) {
         perror("Erro ao ler dados do servidor");
-        EVP_MD_CTX_free(mdctx);
         close(filefd);
         close(client_sock);
         exit(1);
@@ -125,16 +144,7 @@ int main(int argc, char **argv) {
 
     // Calculando o hash MD5 do arquivo recebido
     unsigned char client_md5[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
-    if (EVP_DigestFinal_ex(mdctx, client_md5, &md_len) != 1) {
-        perror("Erro ao finalizar MD5");
-        EVP_MD_CTX_free(mdctx);
-        close(filefd);
-        close(client_sock);
-        exit(1);
-    }
-
-    EVP_MD_CTX_free(mdctx);
+    calculate_md5(filepath, client_md5);
 
     // Verificando a integridade dos dados
     if (memcmp(server_md5, client_md5, EVP_MD_size(EVP_md5())) == 0) {
