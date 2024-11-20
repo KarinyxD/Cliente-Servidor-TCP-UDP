5eab281880425fed67086c3641d58d56b285a112
Kariny Abrahao <kariny_abrahao@hotmail.com>
2024-11-20 13:37:23 -0300

    serverUDP funcionando

---
+++ UDP/serverUDP.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <openssl/evp.h>
+
+char *ROOT="../arquivos_servidor"; // diretorio dos arquivos
+
+void error(const char *msg){
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]){   
+    int sockfd, portno; // socket servidor e porta
+    socklen_t clilen; // tam da estrutura do cliente
+    struct sockaddr_in serv_addr, cli_addr; // endereço IP e porta do servidor e do cliente
+    char buffer[1023];
+    char *filename = "arquivo_binario_10mb.bin"; // Nome do arquivo a ser enviado
+    int filefd; 
+    ssize_t bytes_read;
+    int n;
+
+    if (argc < 2) { // verifica se a porta foi passada como argumento
+        fprintf(stderr,"ERROR, no port provided\n");
+        fprintf(stderr, "Use: %s <PORT>\n", argv[0]);
+        exit(0);
+    }
+
+    // Criando o socket UDP
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    memset(&serv_addr, 0, sizeof(serv_addr)); // Zerando a estrutura serv_addr
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET; //IPv4
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno); // converte o nº da porta
+
+    // Associa o socket à porta e endereço especificados
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+
+    clilen = sizeof(cli_addr);
+
+    // Espera receber a solicitação de um cliente (nn precisa aceitar, apenas receber)
+    n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &cli_addr, &clilen);
+    if (n < 0) 
+        error("ERROR on recvfrom");    
+    printf("Solicitação recebida.\n");
+
+    // Abre o arquivo que será enviado
+    char path[1023];
+    sprintf(path, "%s/%s", ROOT, filename);
+    filefd = open(path, O_RDONLY);
+    if (filefd < 0) {
+        perror("Erro ao abrir o arquivo");
+        close(sockfd);
+        exit(0);
+    }
+
+    // Envia o arquivo em blocos de 1023 bytes
+    while ((bytes_read = read(filefd, buffer, sizeof(buffer))) > 0) {
+        n = sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr *) &cli_addr, clilen);
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            close(filefd);
+            close(sockfd);
+            exit(1);
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("ERROR reading from file");
+    }
+
+    printf("Arquivo enviado com sucesso.\n");
+
+    close(filefd);
+    close(sockfd);
+    return 0;
+}
